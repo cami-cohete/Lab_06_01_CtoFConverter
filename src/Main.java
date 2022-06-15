37ad7bf644e71efd050885b301f9205f539b9cb0
cami-cohete <esquivca@mail.uc.edu>
2022-06-15 17:27:25 -0400

    Final Commit. Finished Lab!

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+
+        int celsius = 0;
+        double fahren = 0;
+        String trash = "";
+
+        System.out.print("Please enter a degree in celsius: ");
+
+        if (in.hasNextInt())
+        {
+            celsius = in.nextInt();
+            in.nextLine();
+        }
+        else
+        {
+            trash = in.nextLine();
+            System.out.println("Please enter a valid number for celsius. The inputted integer was: " + trash);
+            System.out.println("Run the program again and enter a valid amount!");
+            System.exit(0);
+        }
+        fahren = 1.8 * celsius + 32;
+        System.out.println("The degree " + celsius + "C " + "is " + fahren + " fahrenheit.");
+
     }
 }
